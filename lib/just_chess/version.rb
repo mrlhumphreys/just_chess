3e23b936dfeac4b02b9c780f7a46bd898a9373c0
Mark Humphreys <mark@mrlhumphreys.com>
2020-04-23 21:26:48 +1000

    use piece class from board game grid

--- lib/just_chess/version.rb
+++ lib/just_chess/version.rb
@@ -1,4 +1,4 @@
 module JustChess
   # :nodoc:
-  VERSION = "1.0.8"
+  VERSION = "1.0.9"
 end
--- test/just_chess/pieces/piece_test.rb
+++ test/just_chess/pieces/piece_test.rb
@@ -1,20 +1,4 @@
 describe JustChess::Piece do
-  describe 'opponent' do
-    describe 'of player one' do
-      it 'must be two' do
-        piece = JustChess::Piece.new(id: 1, player_number: 1)
-        assert_equal 2, piece.opponent
-      end
-    end
-
-    describe 'of player two' do
-      it 'must be one' do
-        piece = JustChess::Piece.new(id: 1, player_number: 2)
-        assert_equal 1, piece.opponent
-      end
-    end
-  end
-
   describe 'moved' do
     it 'must mark the piece as moved' do
       piece = JustChess::Piece.new(id: 1, player_number: 1, has_moved: false)
@@ -22,35 +6,6 @@ describe JustChess::Piece do
       assert piece.has_not_moved?
       piece.moved
       assert piece.has_moved?
-    end
-  end
-
-  describe 'type' do
-    it 'must be the downcased class name' do
-      piece = JustChess::Pawn.new(id: 1, player_number: 1)
-      assert_equal 'pawn', piece.type
-    end
-  end
-
-  describe 'piece by default' do
-    it 'must not be able to move' do
-      piece = JustChess::Piece.new(id: 1, player_number: 1)
-      refute piece.can_move?(nil, nil, nil)
-    end
-
-    it 'must not have any destinations' do
-      piece = JustChess::Piece.new(id: 1, player_number: 1)
-      assert_empty piece.destinations(nil, nil)
-    end
-
-    it 'must not have any capture squares' do
-      piece = JustChess::Piece.new(id: 1, player_number: 1)
-      assert_empty piece.capture_squares(nil, nil)
-    end
-
-    it 'must not have any move squares' do
-      piece = JustChess::Piece.new(id: 1, player_number: 1)
-      assert_empty piece.move_squares(nil, nil)
     end
   end
 
